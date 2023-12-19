b4a80b58dcbdcbe39da355ad0fb4b97a7014cfb3
shakilmahmud14 <forallworks1414@gmail.com>
2023-12-19 17:07:45 +0600

    ff

--- assist/js/scripts.js
+++ assist/js/scripts.js
@@ -1,34 +1,19 @@
 jQuery(document).ready(function(){
 
+	// navbar toggler js start hare
+
 	jQuery(".toggle-btn").click(function(){
 		jQuery(".main-navbar").slideToggle()
 		jQuery(".menuText").text("Close").css("opacity","1");
 	})
 
-	$.backstretch(
-		[
-			"fixed_bg.jpg",
-			"fixed_bg1.jpg",
-			"fixed_bg2.jpg"
-		], 
-		{
-			duration: 1500, 
-			fade: 700
-	});
+	// navbar js end hare
+	// parallax bg js start hare
 
 	$('.parallax-bgarea').parallax({
 				imageSrc: '../../images/bg-1.jpg'
 			});
 
-
-
-		// for smooth scolling
-
-	// var Scrollbar = window.Scrollbar;
-
-  	// Scrollbar.init(document.querySelector('#my-scrollbar'), {
-  	// 	damping: 0.5,
-  	// });
 
 
 
