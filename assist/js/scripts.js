ea952512cb774a98a406c9f529ec01e7d754ae0d
shakilmahmud14 <forallworks1414@gmail.com>
2024-01-01 19:22:43 +0600

    add

--- assist/js/scripts.js
+++ assist/js/scripts.js
@@ -4,18 +4,37 @@ jQuery(document).ready(function(){
 
 	jQuery(".toggle-btn").click(function(){
 		jQuery(".main-navbar").slideToggle()
-		jQuery(".menuText").text("Close").css("opacity","1");
+		// jQuery(".menuText").text("Close").css("opacity","1");
+		jQuery(".menuText").toggleClass("close-on").css("opacity","1");
 	})
 
 	// navbar js end hare
-	// parallax bg js start hare
 
-	$('.parallax-bgarea').parallax({
-				imageSrc: '../../images/bg-1.jpg'
-			});
-
-
-
+	$('#html').LineProgressbar({
+		percentage:90,
+		radius: '3px',
+		height: '12px',
+	});
+				
+	$('#css').LineProgressbar({
+		percentage:80,
+		radius: '3px',
+		height: '12px',
+		fillBackgroundColor: '#DA4453'
+		});
+				
+	$('#jq').LineProgressbar({
+		percentage:70,
+		radius: '3px',
+		height: '12px',
+		fillBackgroundColor: '#E0C341'
+		});
+	$('#jv').LineProgressbar({
+		percentage:75,
+		radius: '3px',
+		height: '12px',
+		fillBackgroundColor: '#008140'
+		});
 
 
 
