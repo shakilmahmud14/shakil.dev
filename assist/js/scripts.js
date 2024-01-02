ee6bc1ce05cc26a472a4ffe88aa2b44c5cd39431
shakilmahmud14 <forallworks1414@gmail.com>
2024-01-02 20:44:24 +0600

    js add

--- assist/js/scripts.js
+++ assist/js/scripts.js
@@ -9,32 +9,65 @@ jQuery(document).ready(function(){
 	})
 
 	// navbar js end hare
+	$(window).scroll(function(){
+		var htd = $(window).scrollTop();
 
-	$('#html').LineProgressbar({
-		percentage:90,
-		radius: '3px',
-		height: '12px',
-	});
+		if ( htd = 3100) {
+			$('#html').LineProgressbar({
+				percentage:90,
+				radius: '3px',
+				height: '10px',
+				});
+			$('#css').LineProgressbar({
+				percentage:80,
+				radius: '3px',
+				height: '10px',
+				fillBackgroundColor: '#DA4453'
+				});
+						
+			$('#jq').LineProgressbar({
+				percentage:70,
+				radius: '3px',
+				height: '10px',
+				fillBackgroundColor: '#E0C341'
+				});
+			$('#jv').LineProgressbar({
+				percentage:75,
+				radius: '3px',
+				height: '10px',
+				fillBackgroundColor: '#008140'
+				});
+		} else {
+
+		}
+	})
+	
 				
-	$('#css').LineProgressbar({
-		percentage:80,
-		radius: '3px',
-		height: '12px',
-		fillBackgroundColor: '#DA4453'
-		});
-				
-	$('#jq').LineProgressbar({
-		percentage:70,
-		radius: '3px',
-		height: '12px',
-		fillBackgroundColor: '#E0C341'
-		});
-	$('#jv').LineProgressbar({
-		percentage:75,
-		radius: '3px',
-		height: '12px',
-		fillBackgroundColor: '#008140'
-		});
+
+
+
+// top button js hare
+
+	jQuery(".top-button").click(function(){
+		jQuery("html").animate({scrollTop: "0px"}, "fast");
+	})
+
+	jQuery(window).scroll(function(){
+
+		var utd = jQuery(window).scrollTop();
+
+		if ( utd > 500) {
+			jQuery(".top-button").show()
+		} else {
+			jQuery(".top-button").hide()
+		}
+
+
+	})
+	
+	
+
+
 
 
 
